73e35340e27ee872780b6d1ed82a14e88d537538
Yu Xuan <73330308+yuxuansoon@users.noreply.github.com>
2021-03-23 22:03:58 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,44 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  #initialize cache matrix and set it to null
+  cacheMatrix <- NULL
+  #method setMatrix()
+  setMatrix <- function(y) {
+    x <<- y
+    cacheMatrix <<- NULL
+  }
+  #method getMatrix(), returns: matrix.
+  getMatrix() <- function() x 
+  #method setCache()
+  setCache <- function(inverse) cacheMatrix <<- inverse
+  #method getCache()
+  getCache <- function() cacheMatrix
+  #listing named of methods that will be publicly available
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
 }
 
 
-## Write a short comment describing this function
-
+## Function to calculate inverse of 'cache' matrix
+## If inverse is already calculated, it returns the data form cache
+## If inverse is not calcualted, it calculates and returns the matrix inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #Checking for content of the cache matrix
+  cacheMatrix <- x$getCache()
+  #If the content is not null
+  if (!is.null(cacheMatrix)) {
+    message("Load: cache matrix.")
+    return(cacheMatrix)
+  }
+  #If there is no content, get matrix, create, set, update, return cache matrix
+  else {
+    dMatrix <- x$getMatrix()
+    cacheMatrix <- solve(dMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  }
 }
